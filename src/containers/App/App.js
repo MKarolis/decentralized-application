a611516fc713040d4ac3bd146b2e93177f2bee5a
Karolis Medekša <karolis.medeksa@devbridge.com>
2020-12-15 23:00:35 +0200

    Initial DAP UI

---
+++ src/containers/App/App.js
@@ -0,0 +1,112 @@
+import './App.css';
+import React, {Component} from 'react';
+import Web3 from "web3";
+import Application from '../../abis/Application.json';
+import CreateFundraiser from "../CreateFundraiser/CreateFundraiser";
+
+// TODO: Show unsupported page if no ethereum found
+
+class App2 extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        this.loadWeb3();
+        this.connectToNetwork();
+    }
+
+    loadWeb3 = async () => {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable()
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('No ethereum provider found!')
+        }
+    };
+
+    connectToNetwork = async () => {
+        const web3 = window.web3;
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts);
+
+        this.setState({account: accounts[0]});
+
+        const networkId = await web3.eth.net.getId();
+        const networkData = Application.networks[networkId];
+
+        if (networkData) {
+            const application = new web3.eth.Contract(Application.abi, networkData.address);
+
+            application.once('FundraiserCreated', (e, data) => console.log("FundraiserCreated EVENTY: ", data, e));
+
+            this.setState({application});
+            const fundraiserCount = await application.methods.fundraiserCount().call();
+            console.log("FUNDRAISER COUNT", fundraiserCount);
+        } else {
+           window.alert("Application not deployed on selected network!")
+        }
+    };
+
+    getAllFundraisers = () => {
+        const { application } = this.state;
+        application.methods.getAllFundraisers().call().then((value => console.log(value)))
+    };
+
+    createAFundraiser = async () => {
+        const { application, account } = this.state;
+        // const results = await application.methods.createFundraiser('TITLE 2', 0).call();
+        // console.log(results);
+        application.methods.createFundraiser('TITLE 2', 100).send({from: account}, (val, e) => console.log('CALLBACK',e, val))
+            .then(console.log)
+            .catch(console.log);
+    }
+
+    render() {
+        return (
+            <div className="App">
+              <header className="App-header">
+                <p>
+                  Edit <code>src/App.js</code> and save to relodeiu
+                </p>
+                  <button onClick={() => this.getAllFundraisers()}>
+                      CLICK TO GET ALL FUNDRAISERS
+                  </button>
+                  <button onClick={() => this.createAFundraiser()}>
+                      CLICK TO CREATE A FUNDRAISER
+                  </button>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='app-container'>
+                <h1 className='center-header'>Ethereum Fundraiser</h1>
+                <CreateFundraiser/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/containers/CreateFundraiser/CreateFundraiser.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './CreateFundraiser.css';
+
+const CreateFundraiser = () => {
+
+    return(
+        <div className='create-fundraiser-holder'>
+            <button className='create-fundraiser-button'>
+                Create a Fundraiser
+            </button>
+            {/*<Modal show={true} onHide={() => {}}>*/}
+            {/*    <Modal.Header closeButton>*/}
+            {/*        <Modal.Title>Modal heading</Modal.Title>*/}
+            {/*    </Modal.Header>*/}
+            {/*    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>*/}
+            {/*    <Modal.Footer>*/}
+            {/*        <Button variant="secondary" onClick={() => {}}>*/}
+            {/*            Close*/}
+            {/*        </Button>*/}
+            {/*        <Button variant="primary" onClick={() => {}}>*/}
+            {/*            Save Changes*/}
+            {/*        </Button>*/}
+            {/*    </Modal.Footer>*/}
+            {/*</Modal>*/}
+        </div>
+    )
+};
+
+export default CreateFundraiser;
